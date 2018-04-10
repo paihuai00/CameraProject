652a09625536d67e258c3f27a1664298f31ee4c7
cuishuxiang <30729258@qq.com>
2018-04-10 15:05:34 +0800

    添加 图片选择

---
+++ app/src/main/java/com/gzj/cameraproject/ImageUtils.java
@@ -0,0 +1,103 @@
+package com.gzj.cameraproject;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+
+/**
+ * @Created by cuishuxiang
+ * @date 2018/1/11.
+ */
+
+public class ImageUtils {
+
+    private static final String TAG = "ImageUtils";
+
+    /**
+     * Try to return the absolute file path from the given Uri
+     *
+     * @param context
+     * @param uri
+     * @return the file path or null
+     */
+    public static String getRealFilePath(final Context context, final Uri uri ) {
+        if ( null == uri ) return null;
+        final String scheme = uri.getScheme();
+        String data = null;
+        if ( scheme == null )
+            data = uri.getPath();
+        else if ( ContentResolver.SCHEME_FILE.equals( scheme ) ) {
+            data = uri.getPath();
+        } else if ( ContentResolver.SCHEME_CONTENT.equals( scheme ) ) {
+            Cursor cursor = context.getContentResolver().query( uri, new String[] { MediaStore.Images.ImageColumns.DATA }, null, null, null );
+            if ( null != cursor ) {
+                if ( cursor.moveToFirst() ) {
+                    int index = cursor.getColumnIndex( MediaStore.Images.ImageColumns.DATA );
+                    if ( index > -1 ) {
+                        data = cursor.getString( index );
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
+    }
+
+    /**
+     * 通过Uri返回File文件
+     * 注意：通过相机的是类似content://media/external/images/media/97596
+     * 通过相册选择的：file:///storage/sdcard0/DCIM/Camera/IMG_20150423_161955.jpg
+     * 通过查询获取实际的地址
+     * @param uri
+     * @return
+     */
+    public static File getFileByUri(Context context,Uri uri) {
+        String path = null;
+        if ("file".equals(uri.getScheme())) {
+            path = uri.getEncodedPath();
+            if (path != null) {
+                path = Uri.decode(path);
+                ContentResolver cr = context.getContentResolver();
+                StringBuffer buff = new StringBuffer();
+                buff.append("(").append(MediaStore.Images.ImageColumns.DATA).append("=").append("'" + path + "'").append(")");
+                Cursor cur = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[] { MediaStore.Images.ImageColumns._ID, MediaStore.Images.ImageColumns.DATA }, buff.toString(), null, null);
+                int index = 0;
+                int dataIdx = 0;
+                for (cur.moveToFirst(); !cur.isAfterLast(); cur.moveToNext()) {
+                    index = cur.getColumnIndex(MediaStore.Images.ImageColumns._ID);
+                    index = cur.getInt(index);
+                    dataIdx = cur.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    path = cur.getString(dataIdx);
+                }
+                cur.close();
+                if (index == 0) {
+                } else {
+                    Uri u = Uri.parse("content://media/external/images/media/" + index);
+                    System.out.println("temp uri is :" + u);
+                }
+            }
+            if (path != null) {
+                return new File(path);
+            }
+        } else if ("content".equals(uri.getScheme())) {
+            // 4.2.2以后
+            String[] proj = { MediaStore.Images.Media.DATA };
+            Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
+            if (cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                path = cursor.getString(columnIndex);
+            }
+            cursor.close();
+
+            return new File(path);
+        } else {
+            Log.i(TAG, "Uri Scheme:" + uri.getScheme());
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/gzj/cameraproject/MainActivity.java
+++ app/src/main/java/com/gzj/cameraproject/MainActivity.java
@@ -1,13 +1,136 @@
 package com.gzj.cameraproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.engine.impl.GlideEngine;
+import com.zhihu.matisse.internal.entity.CaptureStrategy;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    //图片选择器，请求吗
+    private static final int CHOOSE_IMG_REQUEST = 0;
+    @BindView(R.id.choose_img_btn)
+    Button mChooseImgBtn;
+    @BindView(R.id.show_choose_img)
+    ImageView mShowChooseImg;
+
+    //接收，选择图片返回的uri
+    private List<Uri> chooseImgs = new ArrayList<>();
+    private File imgFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        init();
+
     }
+
+    private void init() {
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //选择图片的返回码
+        if (requestCode == CHOOSE_IMG_REQUEST && resultCode == RESULT_OK) {
+            chooseImgs = Matisse.obtainResult(data);
+            //1 ,拍照得到的图片，需要做处理 (my_images是自己设置的拍照输出路径)
+            // 拍照的存储位置："/sdcard/Pictures/JPEG_20171107_140520.jpg"
+            if (chooseImgs.get(0).getEncodedPath().contains("my_images")) {
+                String imagePath = "/sdcard/Pictures" + chooseImgs.get(0).getEncodedPath().substring(chooseImgs.get(0).getEncodedPath().lastIndexOf("/"));
+                imgFile = new File(imagePath);
+            } else {
+                imgFile = ImageUtils.getFileByUri(this, chooseImgs.get(0));
+            }
+//            updateImage();
+
+            //图片显示到屏幕上
+            Glide.with(this)
+                    .load(imgFile)
+                    .error(R.drawable.ic_default)
+                    .into(mShowChooseImg);
+
+        }
+    }
+
+    @OnClick(R.id.choose_img_btn)
+    public void onViewClicked() {
+        //开启拍照
+
+        MainActivityPermissionsDispatcher.takePhotoWithPermissionCheck(this);
+    }
+
+    //----------------------------权限处理-----------------------------------
+    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void takePhoto() {
+
+        Matisse.from(MainActivity.this)
+                .choose(MimeType.of(MimeType.PNG, MimeType.JPEG))
+                .countable(true)
+                .maxSelectable(1)//最多选择1张
+                .gridExpectedSize(getResources().getDimensionPixelSize(R.dimen.grid_expected_size))//图片显示表格的大小getResources()
+                .capture(true)
+                .captureStrategy(new CaptureStrategy(true, "com.gzj.cameraproject.fileprovider"))
+                .thumbnailScale(0.8f)//缩放比例
+                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                .imageEngine(new GlideEngine())
+                .forResult(CHOOSE_IMG_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @OnShowRationale({Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void takePhotoRationale(final PermissionRequest request) {
+        Log.d(TAG, "takePhotoRationale: ");
+    }
+
+    @OnPermissionDenied({Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void takePhotoPermissionDenied() {
+        Log.d(TAG, "takePhotoPermissionDenied: ");
+    }
+
+    @OnNeverAskAgain({Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void takePhotoPermissionNeverAsk() {
+        Log.d(TAG, "takePhotoPermissionNeverAsk: ");
+    }
+
+
 }
